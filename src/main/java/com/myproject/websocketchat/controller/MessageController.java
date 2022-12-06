fb90975404ed99d6b39c37fe2437d7d8d29777fb
kperevozov <k.perevozov@gmail.com>
2022-12-06 22:47:04 +0300

    5. Added message controller

--- src/main/java/com/myproject/websocketchat/controller/MessageController.java
+++ src/main/java/com/myproject/websocketchat/controller/MessageController.java
@@ -1,25 +1,39 @@
 package com.myproject.websocketchat.controller;
 
 import com.myproject.websocketchat.model.UserMessage;
+import com.myproject.websocketchat.service.MessageService;
 import com.myproject.websocketchat.storage.UserStorage;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class MessageController {
 
-    @Autowired
-    private SimpMessagingTemplate simpMessagingTemplate;
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    private final MessageService messageService;
 
     @MessageMapping("/chat{to}")
-    public void sendMessage(@DestinationVariable String to, UserMessage message){
+    public void sendMessage(@DestinationVariable String to, UserMessage message) {
         System.out.println("handling send message" + message + "to:" + to);
         boolean isExists = UserStorage.getInstance().getUser().contains(to);
-        if (isExists){
+        if (isExists) {
             simpMessagingTemplate.convertAndSend("/topic/messages" + to, message);
         }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserMessage>> getAllUserMessages(@PathVariable Long id) {
+        List<UserMessage> allFromUser = messageService.getAllFromUser(id);
+        return new ResponseEntity<>(allFromUser, HttpStatus.OK);
     }
 }
